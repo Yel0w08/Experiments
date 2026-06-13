23ac7c5b11b0ad8174409c82f50de73d4c308ab5
Yel0w <144521481+Yel0w08@users.noreply.github.com>
2026-06-13 22:32:48 +0200

    Add Silk.NET windowing and refactor renderer
    Add Silk.NET and ImGui package references and refactor rendering/loop logic. Program.cs now constructs ImRender with Simulation and BodyPotitionCalculators and calls ImRender.Initalize(), removing the previous console loop. ImRender was updated to accept Simulation and calculators via constructor, store them as fields, and initialize a Silk.NET IWindow with a Render callback that updates physics each frame and prints state. WindowOptions (size/title) and run loop were added to ImRender.

--- Experiments/N-Body/Program.cs
+++ Experiments/N-Body/Program.cs
@@ -4,20 +4,9 @@ using System.Reflection;
 using N_Body.Math;
 
 
-ImRender _ImRender = new ImRender();
+
 BodyPotitionCalculators _BodyPotitionCalculators = new BodyPotitionCalculators();
 Simulation _simulation = new Simulation();
+ImRender _ImRender = new ImRender(_simulation, _BodyPotitionCalculators);
 
-while (true) 
-{
-    
-    _ImRender.Render();
-    _BodyPotitionCalculators.CalculateForces(_simulation.Bodies);
-    _BodyPotitionCalculators.UpdatePositions(_simulation.Bodies, 0.01);
-    Console.Clear();
-    Console.WriteLine($"Star X: {_simulation.Bodies[0].X:F2}  CoolStar X: {_simulation.Bodies[1].X:F2}");
-
-}
-
-
-Console.ReadLine();
+_ImRender.Initalize();
--- Experiments/N-Body/Render/ImRender.cs
+++ Experiments/N-Body/Render/ImRender.cs
@@ -1,17 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using N_Body.Math;
 using Silk.NET;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace N_Body.Render
 {
     public class ImRender
     {
-        public ImRender()
+        public ImRender(Simulation simulation, BodyPotitionCalculators calc)
         {
-            
+            _simulation = simulation;
+            _calc = calc;
+        }
+
+
+
+        private IWindow _window;
+        private Simulation _simulation;
+        private BodyPotitionCalculators _calc;
+
+        public void Initalize()
+        {
+
+
+
+            var options = WindowOptions.Default;
+            options.Size = new Silk.NET.Maths.Vector2D<int>(800, 600);
+            options.Title = "N-Body";
+
+
+            _window = Window.Create(options);
+            _window.Initialize();
+
+            _window.Render += (dt) =>
+            {
+                _calc.CalculateForces(_simulation.Bodies);
+                _calc.UpdatePositions(_simulation.Bodies, dt);
+                Console.Clear();
+                Console.WriteLine($"Star X: {_simulation.Bodies[0].X:F2}");
+
+            };
+            _window.Run();
+
+
+
         }
         public void Render()
         {
